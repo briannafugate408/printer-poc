396d6dbe6629b9df0c8a04685d5809a47cc327ec
Anthony Washington <anthony@usehyphen.com>
2023-01-17 12:24:39 -0500

    support 1-bit bitmap image printing

--- labelwriter.py
+++ labelwriter.py
@@ -1,10 +1,13 @@
 from socketconn import SocketConnection
+import struct
+
 
 def bencmsg(msg):
     crlf = "\r\n"
     msg_ok = "\r\nOk\r\n"
     message = msg+crlf
     return message.encode()
+
 
 class Labelwriter:
     def __init__(self, ip, port):
@@ -23,9 +26,92 @@ class Labelwriter:
                 time.sleep(1)
                 raise
 
+    def print_bitmap_image_meta_data(self, bmp):
+        print('Type:', bmp.read(2).decode())
+        print('Size: %s' % struct.unpack('I', bmp.read(4)))
+        print('Reserved 1: %s' % struct.unpack('H', bmp.read(2)))
+        print('Reserved 2: %s' % struct.unpack('H', bmp.read(2)))
+        print('Offset: %s' % struct.unpack('I', bmp.read(4)))
+        print('DIB Header Size: %s' % struct.unpack('I', bmp.read(4)))
+        print('Width: %s' % struct.unpack('I', bmp.read(4)))
+        print('Height: %s' % struct.unpack('I', bmp.read(4)))
+        print('Color Planes: %s' % struct.unpack('H', bmp.read(2)))
+        print('Bits per Pixel: %s' % struct.unpack('H', bmp.read(2)))
+        print('Compression Method: %s' % struct.unpack('I', bmp.read(4)))
+        print('Raw Image Size: %s' % struct.unpack('I', bmp.read(4)))
+        print('Horizontal Resolution: %s' % struct.unpack('I', bmp.read(4)))
+        print('Vertical Resolution: %s' % struct.unpack('I', bmp.read(4)))
+        print('Number of Color: %s' % struct.unpack('I', bmp.read(4)))
+        print('Important Color: %s' % struct.unpack('I', bmp.read(4)))
+        print("---------------------------------------")
+
+    # Currently supports one bit bitmap images only
+    def send_print_bitmap_image_command(self, image_path):
+        with open(image_path, "rb") as bmp:
+            # https://en.wikipedia.org/wiki/BMP_file_format
+            self.print_bitmap_image_meta_data(bmp)
+
+            # Getting offset position 10 - aka. where image starts
+            bmp.seek(10, 0)
+            offset = struct.unpack("I", bmp.read(4))[0]
+            # Get the image height and width
+            bmp.seek(18, 0)
+            image_width = struct.unpack("I", bmp.read(4))[0]
+            image_height = struct.unpack("I", bmp.read(4))[0]
+
+            STX = "<STX>"
+            ESC = "<ESC>"
+            ETX = "<ETX>\n"
+
+            IPL = ""
+            IPL += "{}{}c{}".format(STX, ESC, ETX)
+            IPL += "{}{}P{}".format(STX, ESC, ETX)
+            IPL += "{}G1,item;x{};y{};{}".format(STX,
+                                                 image_height, image_width, ETX)
+
+            # Get the size of the image
+            bmp.seek(34, 0)
+            image_size = struct.unpack("I", bmp.read(4))[0]
+            print("Image size:", image_size)
+
+            # Get the number of bytes per row
+            bytes_per_row = int(image_size / image_height)
+            print("Bytes per row:", bytes_per_row)
+
+            # Read picture data in binary
+            bmp.seek(offset, 0)
+
+            binary_row = ""
+            binary_rows = []
+            viewer = []
+
+            row_count = 0
+            for row in range(image_height):
+                for byte in range(bytes_per_row):
+                    # Format HEX byte to binary
+                    binary_row += format(255 - struct.unpack("B",
+                                         bmp.read(1))[0], "08b")
+
+                binary = binary_row[:image_width]
+                binary_rows.append(binary)
+                IPL += "{}u{},{};{}".format(STX, row_count, binary, ETX)
+                row_count += 1
+                binary_row = ""
+
+            IPL += "{}R;{}".format(STX, ETX)
+            self.send_single_command(IPL)
+            self.send_single_command("""
+                <STX><ESC>C<ETX>
+                <STX><ESC>P<ETX>
+                <STX>E8;F8<ETX>
+                <STX>U2;o0,0;c1;w5;h5;<ETX>
+                <STX>R;<ETX>
+                <STX><ESC>E8<ETX>
+                <STX><ETB><ETX>
+            """)
+
     def send_single_command(self, command):
         self._socket.send(bencmsg(command))
-    
 
     def send_print_command(self):
         command = "<STX><ETB><ETX>"
@@ -38,6 +124,3 @@ class Labelwriter:
         <STX>THIS IS THE SAMPLE LABEL<CR><ETX>                           
         <STX><ETB><ETX>             
         """
-        self.send_single_command(command)
-
-
--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from labelwriter import Labelwriter
 
 def main():
     print("Running program to connect to printer!")
-    mylabelwriter = Labelwriter('192.168.1.215', 9100)
-    
+    mylabelwriter = Labelwriter('192.168.1.80', 9100)
+
     # will tell printer to print and confirms a connection
     # mylabelwriter.send_print_command()
 
     # will tell printer to print text only in IPL
-    mylabelwriter.send_text_command()
-    
+    # mylabelwriter.send_text_command()
+
+    # will tell the printer to print the bitmap image
+    mylabelwriter.send_print_bitmap_image_command("one-bit/walk-man.bmp")
+
 
 if __name__ == '__main__':
     main()
